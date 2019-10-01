c40938efb4432ffb2f8278d671a1e23e8205c5eb
David Nguyen <david@netis.be>
2019-10-01 21:04:35 +0200

    trace incoming requests in console

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require('express');
 const app = express();
 const port = process.env.SERVER_PORT || 3000;
 
-app.get('/hello', (req, res) => res.json({ date: new Date().toISOString(), message: 'hello world!'}));
+app.get('/hello', (req, res) => {
+  console.log('GET /hello');
+  res.json({ date: new Date().toISOString(), message: 'hello world!'});
+});
 
-app.get('/health', (req, res) => res.sendStatus(204));
+app.get('/health', (req, res) => {
+  console.log('GET /health');
+  res.sendStatus(204);
+});
 
 app.listen(port, () => console.log(`listening on port ${port}`));
