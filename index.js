19edc6d81e15455e764af763ac98e9a7728587d9
David Nguyen <david@netis.be>
2019-10-03 13:41:45 +0200

    add `/hash` endpoint that generates a SHA256 hash from the current timestamp

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 const express = require('express');
+const crypto = require('crypto');
+const uuid = require('uuid');
+
 const app = express();
 const port = process.env.SERVER_PORT || 3000;
 
 app.get('/hello', (req, res) => {
   console.log('GET /hello');
-  res.json({ date: new Date().toISOString(), message: 'hello world!'});
+  res.json({date: new Date().toISOString(), message: 'hello world!'});
 });
 
 app.get('/health', (req, res) => {
   console.log('GET /health');
   res.sendStatus(204);
 });
+
+app.get('/hash', (req, res) => {
+  const id = uuid.v4().substring(0, 8);
+  const start = Date.now();
+  console.log(`GET /h256 [${id}]`);
+  const hash = crypto
+    .createHmac('sha256', 'My 5uPER 5ecRe7')
+    .update(new Date().toISOString())
+    .digest('base64');
+
+  console.log(`Hash generated for [${id}] in ${Date.now() - start}ms`);
+  res.send(hash)
+});
+
 
 app.listen(port, () => console.log(`listening on port ${port}`));
